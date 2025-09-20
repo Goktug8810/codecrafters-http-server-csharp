80e30d93d4f3fdbe56758907a88300f5c6b87e0d
Goktug8810 <goktug8810@gmail.com>
2025-09-20 14:14:44 +0300

    [any message]

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,13 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket();
+Socket client = server.AcceptSocket();
+
+string response = "HTTP/1.1 200 OK\r\n\r\n";
+byte[] data = Encoding.ASCII.GetBytes(response);
+
+
+client.Send(data);
