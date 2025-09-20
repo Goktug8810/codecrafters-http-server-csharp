dc9986b39fc822faa378d9aa7cd0a63b503faa19
Goktug8810 <goktug8810@gmail.com>
2025-09-20 16:31:50 +0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -4,10 +4,27 @@ using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-Socket client = server.AcceptSocket();
+Socket connection = server.AcceptSocket();
 
-string response = "HTTP/1.1 200 OK\r\n\r\n";
+byte[] buffer = new byte[1024];
+int received = connection.Receive(buffer);
+string request = Encoding.ASCII.GetString(buffer, 0, received);
+
+
+int endOfLine = request.IndexOf("\r\n");
+string requestLine = request[..endOfLine];
+string[] parts = requestLine.Split(' ');
+
+
+string path = parts[1];
+
+string response = (path == "/")
+    ? "HTTP/1.1 200 OK\r\n\r\n"
+    : "HTTP/1.1 404 Not Found\r\n\r\n";
+
+
 byte[] data = Encoding.ASCII.GetBytes(response);
+connection.Send(data);
 
 
-client.Send(data);
+
