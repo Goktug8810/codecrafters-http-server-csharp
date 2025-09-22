9c78b36332586dfcd559110be3563ea2af8d3d4b
Goktug8810 <goktug8810@gmail.com>
2025-09-22 15:03:27 +0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -2,6 +2,10 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 Socket connection = server.AcceptSocket();
@@ -10,13 +14,39 @@ byte[] buffer = new byte[1024];
 int received = connection.Receive(buffer);
 string request = Encoding.ASCII.GetString(buffer, 0, received);
 
-
-int endOfLine = request.IndexOf("\r\n");
-string requestLine = request[..endOfLine];
+int endOfRequestLine = request.IndexOf("\r\n");
+int endOfHeaders = request.IndexOf("\r\n\r\n");
+string requestLine = request[..endOfRequestLine];
 string[] parts = requestLine.Split(' ');
 
-
 string path = parts[1];
+
+string headersSection = "";
+if (endOfHeaders > endOfRequestLine + 2)
+{
+    headersSection = request.Substring(
+        endOfRequestLine + 2,
+        endOfHeaders - (endOfRequestLine + 2));
+}
+
+string? userAgent = null;
+if (!string.IsNullOrEmpty(headersSection))
+{
+    foreach (var line in headersSection.Split("\r\n"))
+    {
+        int colon = line.IndexOf(':');
+        if (colon <= 0) continue;
+
+        string name = line.Substring(0, colon).Trim();
+        string value = line.Substring(colon + 1).Trim();
+
+        if (name.Equals("User-Agent", StringComparison.OrdinalIgnoreCase))
+        {
+            userAgent = value;
+            break;
+        }
+    }
+}
 
 string response;
 
@@ -27,7 +57,18 @@ if (path == "/")
 else if (path.StartsWith("/echo/"))
 {
     string body = path.Substring("/echo/".Length);
-    int length = Encoding.ASCII.GetByteCount(body); 
+    int length = Encoding.ASCII.GetByteCount(body);
+    response =
+        "HTTP/1.1 200 OK\r\n" +
+        "Content-Type: text/plain\r\n" +
+        $"Content-Length: {length}\r\n" +
+        "\r\n" +
+        body;
+}
+else if (path == "/user-agent")
+{
+    string body = userAgent ?? "";
+    int length = Encoding.ASCII.GetByteCount(body);
     response =
         "HTTP/1.1 200 OK\r\n" +
         "Content-Type: text/plain\r\n" +
@@ -39,7 +80,6 @@ else
 {
     response = "HTTP/1.1 404 Not Found\r\n\r\n";
 }
-
 
 byte[] data = Encoding.ASCII.GetBytes(response);
 connection.Send(data);
